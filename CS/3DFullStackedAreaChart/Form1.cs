9a4d73cd02d1e8f2fb86b3b7b7c5c489ee82b555
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/3DFullStackedAreaChart/Form1.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+// ...
+
+namespace _DFullStackedAreaChart {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create an empty chart.
+            ChartControl fullStackedArea3DChart = new ChartControl();
+
+            // Create two series of the FullStackedArea3D view type.
+            Series series1 = new Series("Series 1", ViewType.FullStackedArea3D);
+            Series series2 = new Series("Series 2", ViewType.FullStackedArea3D);
+
+            // Populate both series with points.
+            series1.Points.Add(new SeriesPoint("A", 80));
+            series1.Points.Add(new SeriesPoint("B", 20));
+            series1.Points.Add(new SeriesPoint("C", 50));
+            series1.Points.Add(new SeriesPoint("D", 30));
+            series2.Points.Add(new SeriesPoint("A", 40));
+            series2.Points.Add(new SeriesPoint("B", 60));
+            series2.Points.Add(new SeriesPoint("C", 20));
+            series2.Points.Add(new SeriesPoint("D", 80));
+
+            // Add the series to the chart.
+            fullStackedArea3DChart.Series.AddRange(new Series[] {
+                series1,
+                series2});
+
+            // Adjust the view-type-specific options of the series.
+            ((FullStackedArea3DSeriesView)series1.View).Transparency = 20;
+            ((FullStackedArea3DSeriesView)series2.View).Transparency = 60;
+
+            // Access the diagram's options.
+            ((XYDiagram3D)fullStackedArea3DChart.Diagram).ZoomPercent = 110;
+
+            // Add a title to the chart and hide the legend.
+            ChartTitle chartTitle1 = new ChartTitle();
+            chartTitle1.Text = "3D Full Stacked Area Chart";
+            fullStackedArea3DChart.Titles.Add(chartTitle1);
+            fullStackedArea3DChart.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
+
+            // Add the chart to the form.
+            fullStackedArea3DChart.Dock = DockStyle.Fill;
+            this.Controls.Add(fullStackedArea3DChart);
+        }
+
+    }
+}
+
---
+++ CS/3DFullStackedAreaChart/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("3DFullStackedAreaChart")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("3DFullStackedAreaChart")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("69cbaf3a-b576-44e9-8564-8a20ed557324")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
